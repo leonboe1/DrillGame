d1d91b84c6ffa1d5a7311c22a415ccb90bc6087b
Leon Böttger <git@leonboettger.com>
2023-06-03 12:19:15 +0200

    Drill can only move down

--- Assets/Scenes/MoveDown.cs
+++ Assets/Scenes/MoveDown.cs
@@ -17,9 +17,29 @@ public class MoveDown : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        // Move forward according to curve direction
         float curveDirection = Input.GetKey(KeyCode.Space) ? 1 : -1;
-
         transform.Rotate(Vector3.forward * curveDirection * curveSpeed * Time.deltaTime);
-        transform.position += transform.up * -speed * Time.deltaTime;
+        Vector3 transformedPosition = transform.up * -speed * Time.deltaTime;
+
+        // Don't allow to go up
+        if(transformedPosition.y > 0) {
+            transformedPosition.y = 0;
+        }
+
+        transform.position += transformedPosition;
+
+
+        // Don't allow rotation to point towards top
+        float zRotation = transform.rotation.eulerAngles.z;
+
+        if(zRotation > 90 && zRotation < 270) {
+            if(Mathf.Abs(zRotation-90) < Mathf.Abs(zRotation-270)) {
+                transform.rotation = Quaternion.Euler(0, 0, 90);
+            }
+            else {
+                transform.rotation = Quaternion.Euler(0, 0, 270);
+            }
+        }
     }
 }
