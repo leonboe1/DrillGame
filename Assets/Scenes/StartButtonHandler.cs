4355fd550d9bd10d25f94d5789e5a598a2f18ff1
Leon Böttger <leon.boettger@icloud.com>
2023-06-13 08:46:39 +0200

    Add Start Button

---
+++ Assets/Scenes/StartButtonHandler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartButtonHandler : MonoBehaviour
+{
+
+    public Button startButton;
+    public Text startButtonText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(button_pressed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void button_pressed()
+    {
+        startButtonText.text = "Loading...";
+        SceneManager.LoadScene("MainGame");
+    }
+}
