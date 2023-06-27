a32ebf4f892b7109742d8177c6bc543941f4a19f
Leon Böttger <leon.boettger@icloud.com>
2023-06-27 13:15:15 +0200

    Add difficulty level

---
+++ Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    public Button difficultyButton;
+    public Text difficultyButtonText;
+    
+    // 0 = easy
+    // 1 = normal
+    // 2 = hard
+    public static int difficultyLevel = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadDifficultyString();
+        difficultyButton.onClick.AddListener(button_pressed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space)) {
+            button_pressed();
+        }
+    }
+
+    public void button_pressed()
+    {
+        difficultyLevel = (difficultyLevel + 1) % 3;
+        LoadDifficultyString();
+    }
+    
+    void LoadDifficultyString()
+    {
+        var label = "Difficulty: ";
+        
+        switch(difficultyLevel) {
+            case 0:
+                label = label + " Easy";
+                break;
+            case 1:
+                label = label + " Normal";
+                break;
+            case 2:
+                label = label + " Hard";
+                break;
+        }
+        
+        difficultyButtonText.text = label;
+    }
+}
